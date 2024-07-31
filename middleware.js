66a31030936d463555571ed61e2373c4217f615c
Nikola Furundzic <nfurundzic@Nikolas-MacBook-Pro.local>
2024-07-31 09:11:31 -0400

    TEST

--- middleware.js
+++ middleware.js
@@ -3,46 +3,69 @@ import { cookies } from "next/headers"
 import { jwtVerify } from 'jose';
 
 
-export async function middleware(request, response) {
+export async function middleware(request) {
 
-  
+
   const url = request.nextUrl.clone();
+  const pathname = url.pathname;
   const cookieStore = cookies();
   const jwtToken = cookieStore.get('jwt_token')?.value;
+  const paths = ['/main', '/', '/login'];
 
-  if(url.pathname.includes('/api/')) {
-    return NextResponse.next();
-  }
-
-  if(!jwtToken && url.pathname !== '/login') {
-    url.pathname = '/login';
-    return NextResponse.redirect(url);
-  } 
-
-  try {
-    const { payload } = await jwtVerify(jwtToken, new TextEncoder().encode(process.env.JWT_SECRET));
-    console.log('---------- PATH NAME ----- ' , url.pathname, ' ---- PAYLOAD ---- ', payload)
-      if(payload?.email && url.pathname !== '/main') {
-        if(!response.finished) {
-
-          url.pathname = '/main';
-          return NextResponse.redirect(url);
-        }
-      } 
-      
-      return NextResponse.next();
-
-  } catch (error) {
-    if(url.pathname !== '/login') {
-      url.pathname = '/login';
-      return NextResponse.redirect(url);
+  if (paths.includes(pathname)) {
+    
+    try {
+      const { payload } = await jwtVerify(jwtToken, new TextEncoder().encode(process.env.JWT_SECRET));
+      if (!payload?.email) {
+        return NextResponse.redirect(new URL('/login', request.url));
+      }
+      if (payload?.email && !pathname.includes('/main')) {
+        return NextResponse.redirect(new URL('/main', request.url));
+      }
+    } catch (error) {
+      return NextResponse.redirect(new URL('/login', request.url));
     }
   }
+
   return NextResponse.next();
+
+
+  
+  // const url = request.nextUrl.clone();
+  // const cookieStore = cookies();
+  // const jwtToken = cookieStore.get('jwt_token')?.value;
+
+  // if(url.pathname.includes('/api/')) {
+  //   return NextResponse.next();
+  // }
+
+  // if(!jwtToken && url.pathname !== '/login') {
+  //   url.pathname = '/login';
+  //   return NextResponse.redirect(url);
+  // } 
+
+  // try {
+  //   const { payload } = await jwtVerify(jwtToken, new TextEncoder().encode(process.env.JWT_SECRET));
+  //   console.log('---------- PATH NAME ----- ' , url.pathname, ' ---- PAYLOAD ---- ', payload)
+  //     if(payload?.email && url.pathname !== '/main') {
+  //         url.pathname = '/main';
+  //         return NextResponse.redirect(url);
+  //     } 
+      
+  //     return NextResponse.next();
+
+  // } catch (error) {
+  //   if(url.pathname !== '/login') {
+  //     url.pathname = '/login';
+  //     return NextResponse.redirect(url);
+  //   }
+  // }
+  // return NextResponse.next();
 }
 
 export const config = {
   matcher: [
-    '/((?!_next/static|_next/image|favicon.ico).*)',
+    // '/((?!_next/static|_next/image|favicon.ico).*)', 
+    '/main', '/:path*', '/'
   ],
 };
